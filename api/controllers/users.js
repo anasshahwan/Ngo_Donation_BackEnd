bdc64d8bc8f6142aabaa00aee336637f38e14a79
Faatimah Hossain <80355142+Fhossa09@users.noreply.github.com>
2021-04-03 17:00:19 -0400

    Update users.js
    Did more testing... should work now.

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -30,3 +30,29 @@ exports.delete_user = (req, res, next) => {
        });
      });
 }
+
+exports.update_user = (req, res, next) => {
+  const id = req.params.userId;
+  console.log(id)
+  User.findOneAndUpdate({_id:id}, 
+    {
+      $set: {
+        firstname:req.body.firstname,
+        lastname:req.body.lastname,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.body.role
+      }
+    })
+     .exec()
+     .then(result => {
+       res.status(200).json(result);
+     })
+     .catch(err => {
+       console.log(err);
+       res.status(500).json({
+         error:err
+       });
+     });
+}
+
